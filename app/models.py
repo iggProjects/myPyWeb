70e73e6c5fa31e6ce3c35bfd46216890c0846e85
InakiGainza <igg.git.h@gmail.com>
2023-03-07 10:23:50 +0100

    add libraries classes and

---
+++ app/models.py
@@ -0,0 +1,28 @@
+# chapter 4
+from app import db
+
+# Posts database table and relationship
+from datetime import datetime
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+
+    # Posts database table and relationship
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+    
+# Posts database table and relationship
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Post {}>'.format(self.body)
+        
