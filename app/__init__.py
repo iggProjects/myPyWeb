8765c8110780ed05fd0c509d63bbbe0247ab900c
InakiGainza <igg.git.h@gmail.com>
2023-03-07 10:23:31 +0100

    add libraries classes and

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,17 @@
 from flask import Flask
 from config import Config
 
+# Flask-SQLAlchemy and Flask-Migrate initialization
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+
 app = Flask(__name__)
 app.config.from_object(Config)
 
-from app import routes
+# db info
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+# add models (classes) for db 
+from app import routes, models
